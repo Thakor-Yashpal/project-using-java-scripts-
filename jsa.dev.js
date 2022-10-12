4dcba5fb67c1ef8a787583a4607b7b047f87bd8a
Thakor Yashpal <thakoryashpal755@gmail.com>
2022-10-12 16:32:55 +0530

    Add files via upload

---
+++ jsa.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log("Itachi"); //info
+
+console.info(' how is best anime chapter'); //warn
+
+console.warn('who are you ?'); //error
+
+console.error('what you want...!'); //table
+
+var stundet = {
+  name: 'Thakor Yashpal'
+};
+console.table(stundet);
+//# sourceMappingURL=jsa.dev.js.map
